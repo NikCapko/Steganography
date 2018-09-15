a3eabe36d0e78e973855a166be6ff88a8d23b5bc
NikCapko <nik.capko@mail.ru>
2018-09-15 20:53:49 +0300

    add new method

--- Steganography/Form1.Designer.cs
+++ Steganography/Form1.Designer.cs
@@ -29,17 +29,21 @@
         private void InitializeComponent()
         {
             this.btnEncrypt = new System.Windows.Forms.Button();
-            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.pbImage1 = new System.Windows.Forms.PictureBox();
             this.btnDecrypt = new System.Windows.Forms.Button();
             this.tbText = new System.Windows.Forms.TextBox();
             this.btnSave = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.pbImage2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage2)).BeginInit();
             this.SuspendLayout();
             // 
             // btnEncrypt
             // 
             this.btnEncrypt.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnEncrypt.Location = new System.Drawing.Point(558, 146);
+            this.btnEncrypt.Location = new System.Drawing.Point(607, 139);
             this.btnEncrypt.Name = "btnEncrypt";
             this.btnEncrypt.Size = new System.Drawing.Size(211, 71);
             this.btnEncrypt.TabIndex = 0;
@@ -47,22 +51,22 @@
             this.btnEncrypt.UseVisualStyleBackColor = true;
             this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
             // 
-            // pbImage
+            // pbImage1
             // 
-            this.pbImage.BackColor = System.Drawing.Color.White;
-            this.pbImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pbImage.Location = new System.Drawing.Point(12, 129);
-            this.pbImage.Name = "pbImage";
-            this.pbImage.Size = new System.Drawing.Size(509, 354);
-            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pbImage.TabIndex = 1;
-            this.pbImage.TabStop = false;
-            this.pbImage.Click += new System.EventHandler(this.pbImage_Click);
+            this.pbImage1.BackColor = System.Drawing.Color.White;
+            this.pbImage1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImage1.Location = new System.Drawing.Point(12, 190);
+            this.pbImage1.Name = "pbImage1";
+            this.pbImage1.Size = new System.Drawing.Size(276, 357);
+            this.pbImage1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbImage1.TabIndex = 1;
+            this.pbImage1.TabStop = false;
+            this.pbImage1.Click += new System.EventHandler(this.pbImage1_Click);
             // 
             // btnDecrypt
             // 
             this.btnDecrypt.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnDecrypt.Location = new System.Drawing.Point(558, 223);
+            this.btnDecrypt.Location = new System.Drawing.Point(607, 216);
             this.btnDecrypt.Name = "btnDecrypt";
             this.btnDecrypt.Size = new System.Drawing.Size(211, 71);
             this.btnDecrypt.TabIndex = 2;
@@ -77,13 +81,13 @@
             this.tbText.Multiline = true;
             this.tbText.Name = "tbText";
             this.tbText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.tbText.Size = new System.Drawing.Size(776, 96);
+            this.tbText.Size = new System.Drawing.Size(828, 96);
             this.tbText.TabIndex = 3;
             // 
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnSave.Location = new System.Drawing.Point(558, 364);
+            this.btnSave.Location = new System.Drawing.Point(607, 357);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(211, 71);
             this.btnSave.TabIndex = 4;
@@ -91,19 +95,55 @@
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
+            // pbImage2
+            // 
+            this.pbImage2.BackColor = System.Drawing.Color.White;
+            this.pbImage2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImage2.Location = new System.Drawing.Point(306, 190);
+            this.pbImage2.Name = "pbImage2";
+            this.pbImage2.Size = new System.Drawing.Size(276, 357);
+            this.pbImage2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbImage2.TabIndex = 5;
+            this.pbImage2.TabStop = false;
+            this.pbImage2.Click += new System.EventHandler(this.pbImage2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(92, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 26);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Исходное";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(370, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 26);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Полученное";
+            // 
             // fmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 495);
+            this.ClientSize = new System.Drawing.Size(852, 559);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbImage2);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.tbText);
             this.Controls.Add(this.btnDecrypt);
-            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.pbImage1);
             this.Controls.Add(this.btnEncrypt);
             this.Name = "fmMain";
             this.Text = "Steganography";
-            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,10 +152,13 @@
         #endregion
 
         private System.Windows.Forms.Button btnEncrypt;
-        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.PictureBox pbImage1;
         private System.Windows.Forms.Button btnDecrypt;
         private System.Windows.Forms.TextBox tbText;
         private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pbImage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Steganography/Form1.cs
+++ Steganography/Form1.cs
@@ -6,17 +6,36 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace Steganography
 {
-    public partial class fmMain : System.Windows.Forms.Form
+    public partial class fmMain : Form
     {
         public fmMain()
         {
             InitializeComponent();
         }
 
-        private void pbImage_Click(object sender, EventArgs e)
+        private void pbImage1_Click(object sender, EventArgs e)
+        {
+            pbImage1.Image = (Image)openImage();
+            //MessageBox.Show(((int)"А"[0]).ToString());
+
+
+
+            //byte[] byteArray = new byte[(int)Math.Ceiling((double)a.Length / 8)];
+            //a.CopyTo(byteArray, 0);
+            //var str = Encoding.Unicode.GetString(byteArray);
+        }
+
+        private void pbImage2_Click(object sender, EventArgs e)
+        {
+            pbImage2.Image = (Image)openImage();
+        }
+
+        Bitmap openImage()
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Image Files(*.BMP)|*.bmp";
@@ -25,7 +44,7 @@ namespace Steganography
                 try
                 {
                     Bitmap bitmap = new Bitmap(openFileDialog.FileName);
-                    pbImage.Image = (Image)bitmap;
+                    return bitmap;
                 }
                 catch
                 {
@@ -33,38 +52,46 @@ namespace Steganography
                         MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
+
+            return null;
         }
 
         private void btnEncrypt_Click(object sender, EventArgs e)
         {
-            if (pbImage.Image != null)
+            if (pbImage1.Image != null)
             {
-                Bitmap bitmap = new Bitmap(pbImage.Image);
+                Bitmap bitmap = new Bitmap(pbImage1.Image);
 
                 if (!tbText.Text.Equals(""))
                 {
                     string text = tbText.Text.Normalize();
-                    int length = text.Length;
-                    int iText = 0;
+                    byte[] strBytes = Encoding.Unicode.GetBytes(text);
+                    string s = BitConverter.ToString(strBytes, 0);
+                    BitArray bitArray = new BitArray(strBytes);
+                    int length = bitArray.Length;
+                    int index = 0;
                     if (length <= bitmap.Height * bitmap.Width)
                     {
-                        for (int i = 0; i < bitmap.Height; i++)
+                        for (int i = 0; i < bitmap.Width; i++)
                         {
-                            for (int j = 0; j < bitmap.Width; j++)
+                            for (int j = 0; j < bitmap.Height; j++)
                             {
-                                if (length > iText)
+                                if (length - 3 > index)
                                 {
                                     Color color1 = bitmap.GetPixel(i, j);
-                                    Color color2 = Color.FromArgb(color1.A, color1.R, color1.G, (int)text[iText]);
+                                    int r = (color1.R == 255 && Convert.ToInt32(bitArray.Get(index)) == 1) ? 254 : color1.R + Convert.ToInt32(bitArray.Get(index));
+                                    int g = (color1.G == 255 && Convert.ToInt32(bitArray.Get(index + 1)) == 1) ? 254 : color1.G + Convert.ToInt32(bitArray.Get(index + 1));
+                                    int b = (color1.B == 255 && Convert.ToInt32(bitArray.Get(index + 2)) == 1) ? 254 : color1.B + Convert.ToInt32(bitArray.Get(index + 2));
+                                    Color color2 = Color.FromArgb(color1.A, r, g, b);
                                     bitmap.SetPixel(i, j, color2);
-                                    iText++;
+                                    index += 3;
                                 }
                                 else
                                 {
                                     Color color1 = bitmap.GetPixel(i, j);
-                                    Color color2 = Color.FromArgb(color1.A, 13, color1.G, color1.B);
+                                    Color color2 = Color.FromArgb(color1.A, (color1.R >= 2 ? color1.R - 2 : color1.R + 2), color1.G, color1.B);
                                     bitmap.SetPixel(i, j, color2);
-                                    pbImage.Image = (Image)bitmap;
+                                    pbImage2.Image = (Image)bitmap;
                                     return;
                                 }
                             }
@@ -93,26 +120,53 @@ namespace Steganography
         {
             tbText.Text = "";
 
-            if (pbImage.Image != null)
+            if (pbImage1.Image != null && pbImage2.Image != null)
             {
-                Bitmap bitmap = new Bitmap(pbImage.Image);
 
-                for (int i = 0; i < bitmap.Height; i++)
+                Bitmap bitmap1 = new Bitmap(pbImage1.Image);
+                Bitmap bitmap2 = new Bitmap(pbImage2.Image);
+
+                string bitString = "";
+
+                ArrayList arrayList = new ArrayList();
+
+                if (bitmap1.Width == bitmap2.Width && bitmap1.Height == bitmap2.Height)
                 {
-                    for (int j = 0; j < bitmap.Width; j++)
+                    for (int i = 0; i < bitmap1.Width; i++)
                     {
-                        if (bitmap.GetPixel(i, j).R == 13)
+                        for (int j = 0; j < bitmap1.Height; j++)
                         {
-                            return;
-                        }
-                        else
-                        {
-                            Color color = bitmap.GetPixel(i, j);
-                            tbText.Text += ((char)color.B).ToString();
+                            if (Math.Abs(bitmap1.GetPixel(i, j).R - bitmap2.GetPixel(i, j).R) == 2)
+                            {
+                                byte[] byteArray = Enumerable.Range(0, bitString.Length / 8).
+                                    Select(pos => Convert.ToByte(bitString.Substring(pos * 8, 8), 2)).ToArray();
+                                BitArray bitArray = new BitArray(arrayList.Count);
+                                var str = Encoding.Unicode.GetString(byteArray);
+                                tbText.Text = str;
+                                return;
+                            }
+                            else
+                            {
+                                int r = Math.Abs(bitmap1.GetPixel(i, j).R - bitmap2.GetPixel(i, j).R);
+                                int g = Math.Abs(bitmap1.GetPixel(i, j).G - bitmap2.GetPixel(i, j).G);
+                                int b = Math.Abs(bitmap1.GetPixel(i, j).B - bitmap2.GetPixel(i, j).B);
+
+                                //arrayList.Add(r == 1 ? true : false);
+                                //arrayList.Add(g == 1 ? true : false);
+                                //arrayList.Add(b == 1 ? true : false);
+
+                                bitString += r.ToString() + g.ToString() + b.ToString();
+                            }
                         }
                     }
                 }
+                else
+                {
+                    MessageBox.Show("Размеры изображений не совпадают", "Ошибка",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
             }
+
             else
             {
                 MessageBox.Show("Вы не выбрали изображение", "Ошибка",
@@ -122,7 +176,7 @@ namespace Steganography
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            if (pbImage.Image != null)
+            if (pbImage2.Image != null)
             {
                 SaveFileDialog savedialog = new SaveFileDialog();
                 savedialog.Title = "Сохранить картинку как...";
@@ -134,7 +188,7 @@ namespace Steganography
                 {
                     try
                     {
-                        pbImage.Image.Save(savedialog.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                        pbImage2.Image.Save(savedialog.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
                     }
                     catch
                     {
